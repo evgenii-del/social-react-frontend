0230e1c5f65b9df980e541075ca886e28e8546b7
Khasanov <zkhasanovv@gmail.com>
2020-08-19 17:55:17 +0300

    Class component for Users page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,74 +1,107 @@
 import React from 'react';
 import './Users.css';
+import * as axios from "axios";
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                name: 'Evgenii',
-                photo: 'https://images.pexels.com/photos/2884377/pexels-photo-2884377.jpeg',
-                status: 'I like Python',
-                location: {
-                    city: 'Kiev',
-                    country: 'Ukraine'
-                },
-                followed: true
-            },
-            {
-                id: 2,
-                name: 'Filip',
-                photo: 'https://images.pexels.com/photos/4939074/pexels-photo-4939074.jpeg',
-                status: 'I like Java',
-                location: {
-                    city: 'Russia',
-                    country: 'Moscow'
-                },
-                followed: false
-            },
-            {
-                id: 3,
-                name: 'Bob',
-                photo: 'https://images.pexels.com/photos/2480334/pexels-photo-2480334.jpeg',
-                status: 'I like JS',
-                location: {
-                    city: 'LA',
-                    country: 'USA'
-                },
-                followed: true
-            }
-        ])
+// const Users = (props) => {
+//     let showUsers = () => {
+//         if (props.users.length === 0) {
+//
+//             axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+//                 props.setUsers(response.data);
+//             })
+//         }
+//     }
+//
+//     return (
+//         <div>
+//             <button onClick={showUsers}>Show</button>
+//             {
+//                 props.users.map(user => <div className="users">
+//                         <div className="users-left" key={user.id}>
+//                             <div className="users-img">
+//                                 <img src='https://images.pexels.com/photos/2480334/pexels-photo-2480334.jpeg'/>
+//                             </div>
+//                             <div className="users-btn">
+//                                 {user.followed ?
+//                                     <a onClick={() => {
+//                                         props.unfollow(user.id)
+//                                     }}>Unfollow</a> :
+//                                     <a onClick={() => {
+//                                         props.follow(user.id)
+//                                     }}>Follow</a>
+//                                 }
+//                             </div>
+//                         </div>
+//                         <div className="users-right">
+//                             <div>
+//                                 {user.name} - {user.website} <br/>
+//                                 {user.email}
+//                             </div>
+//                             <div>
+//                                 {user.address.city}, {user.address.street}
+//                             </div>
+//                         </div>
+//                     </div>
+//                 )
+//             }
+//         </div>
+//     )
+// }
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
+        if (this.props.users.length === 0) {
+            axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+                this.props.setUsers(response.data);
+            })
+        }
     }
 
-    return (
-        props.users.map(user =>
-            <div className="users">
-                <div className="users-left" key={user.id}>
-                    <div className="users-img">
-                        <img src={user.photo}/>
-                    </div>
-                    <div className="users-btn">
-                        {user.followed ?
-                            <a onClick={() => {
-                                props.unfollow(user.id)
-                            }}>Unfollow</a> :
-                            <a onClick={() => {
-                                props.follow(user.id)
-                            }}>Follow</a>
-                        }
-                    </div>
-                </div>
-                <div className="users-right">
-                    <div>
-                        {user.name} - {user.status}
-                    </div>
-                    <div>
-                        {user.location.country}, {user.location.city}
-                    </div>
-                </div>
+    // getUsers = () => {
+    //     if (this.props.users.length === 0) {
+    //         axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+    //             this.props.setUsers(response.data);
+    //         })
+    //     }
+    // }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(user => <div className="users">
+                            <div className="users-left" key={user.id}>
+                                <div className="users-img">
+                                    <img src='https://images.pexels.com/photos/2480334/pexels-photo-2480334.jpeg'/>
+                                </div>
+                                <div className="users-btn">
+                                    {user.followed ?
+                                        <a onClick={() => {
+                                            this.props.unfollow(user.id)
+                                        }}>Unfollow</a> :
+                                        <a onClick={() => {
+                                            this.props.follow(user.id)
+                                        }}>Follow</a>
+                                    }
+                                </div>
+                            </div>
+                            <div className="users-right">
+                                <div>
+                                    {user.name} - {user.website} <br/>
+                                    {user.email}
+                                </div>
+                                <div>
+                                    {user.address.city}, {user.address.street}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         )
-    )
+    }
 }
 
 export default Users;
