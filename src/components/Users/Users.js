43f6a606153d7e750aa0fe7bb2d6a80cb9f27961
Khasanov <zkhasanovv@gmail.com>
2020-08-29 18:53:19 +0300

    Changed presentation component Users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,107 +1,39 @@
 import React from 'react';
 import './Users.css';
-import * as axios from "axios";
 
-// const Users = (props) => {
-//     let showUsers = () => {
-//         if (props.users.length === 0) {
-//
-//             axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
-//                 props.setUsers(response.data);
-//             })
-//         }
-//     }
-//
-//     return (
-//         <div>
-//             <button onClick={showUsers}>Show</button>
-//             {
-//                 props.users.map(user => <div className="users">
-//                         <div className="users-left" key={user.id}>
-//                             <div className="users-img">
-//                                 <img src='https://images.pexels.com/photos/2480334/pexels-photo-2480334.jpeg'/>
-//                             </div>
-//                             <div className="users-btn">
-//                                 {user.followed ?
-//                                     <a onClick={() => {
-//                                         props.unfollow(user.id)
-//                                     }}>Unfollow</a> :
-//                                     <a onClick={() => {
-//                                         props.follow(user.id)
-//                                     }}>Follow</a>
-//                                 }
-//                             </div>
-//                         </div>
-//                         <div className="users-right">
-//                             <div>
-//                                 {user.name} - {user.website} <br/>
-//                                 {user.email}
-//                             </div>
-//                             <div>
-//                                 {user.address.city}, {user.address.street}
-//                             </div>
-//                         </div>
-//                     </div>
-//                 )
-//             }
-//         </div>
-//     )
-// }
-
-class Users extends React.Component {
-    constructor(props) {
-        super(props);
-
-        if (this.props.users.length === 0) {
-            axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
-                this.props.setUsers(response.data);
-            })
-        }
-    }
-
-    // getUsers = () => {
-    //     if (this.props.users.length === 0) {
-    //         axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
-    //             this.props.setUsers(response.data);
-    //         })
-    //     }
-    // }
-
-    render() {
-        return (
-            <div>
-                {
-                    this.props.users.map(user => <div className="users">
-                            <div className="users-left" key={user.id}>
-                                <div className="users-img">
-                                    <img src='https://images.pexels.com/photos/2480334/pexels-photo-2480334.jpeg'/>
-                                </div>
-                                <div className="users-btn">
-                                    {user.followed ?
-                                        <a onClick={() => {
-                                            this.props.unfollow(user.id)
-                                        }}>Unfollow</a> :
-                                        <a onClick={() => {
-                                            this.props.follow(user.id)
-                                        }}>Follow</a>
-                                    }
-                                </div>
+const Users = (props) => {
+    return (
+        <div>
+            {
+                props.users.map(user => <div className="users">
+                        <div className="users-left" key={user.id}>
+                            <div className="users-img">
+                                <img src='https://images.pexels.com/photos/2480334/pexels-photo-2480334.jpeg'/>
                             </div>
-                            <div className="users-right">
-                                <div>
-                                    {user.name} - {user.website} <br/>
-                                    {user.email}
-                                </div>
-                                <div>
-                                    {user.address.city}, {user.address.street}
-                                </div>
+                            <div className="users-btn">
+                                {user.followed ? <input type="submit" value="UNFOLLOW" onClick={() => {
+                                    props.unfollow(user.id)
+                                }}/> : <input type="submit" value="FOLLOW" onClick={() => {
+                                    props.follow(user.id)
+                                }}/>
+                                }
                             </div>
                         </div>
-                    )
-                }
-            </div>
-        )
-    }
+                        <div className="users-right">
+                            <div>
+                                {user.name} - {user.website} <br/>
+                                {user.email}
+                            </div>
+                            <div>
+                                {user.address.city}, {user.address.street}
+                            </div>
+
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import Users from './Users';
 import {connect} from "react-redux";
 import {followActionCreator, setUsersActionCreator, unfollowActionCreator} from "../../redux/users-reducer";
+import * as axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+            this.props.setUsers(response.data);
+        })
+    }
+
+    render() {
+        return (
+            <Users users={this.props.users} follow={this.props.follow} unfollow={this.props.unfollow}/>
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -23,5 +38,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
